4622e4732d0bc5f90e15e3432f83e16b43f64778
Kashif <kashif.khan@dubaiairports.ae>
2020-09-05 18:42:44 +0400

    adding dccore file

---
+++ dcops.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.8
+
+from get_ep_details3 import main as ep
+from get_fault_details_v2 import main as fd
+
+def main():
+    
+    print('#'*50)
+    print('Welcome to Data Center Operations Application')
+    print('#'*50)
+    print('\n')
+
+    while True:
+
+        print('1 : Endpoint related')
+        print('2 : Faults details')
+        print('0 : Exit')
+        main_operation = input('\nChoose number to select type of operation : ')
+
+        if main_operation == '0':
+            break
+        if main_operation == '1':
+            
+            ep()
+
+        if main_operation == '2':
+            
+            fd()
+            
+
+if __name__ == '__main__':
+    main()
