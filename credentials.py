459f7dab908080a9b34e92bdad06c75ba9fda0d5
me-kashif <55882588+me-kashif@users.noreply.github.com>
2020-12-27 14:50:29 +0400

    changed credential files

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 credentials = {
-    "username": "kashif",
-    "password": "Huawei123+",
-    "apic_ip": "https://172.29.188.151/"
+    "username": "admin",
+    "password": "ciscopsdt",
+    "apic_ip": "https://sandboxapicdc.cisco.com/"
 }
